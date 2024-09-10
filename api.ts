f7d70667b5a0846c564fe5ac9f93d3e6daea2302
Hazeem5 <hazeemaulakh5@gmail.com.pk>
2024-09-10 15:12:24 +0500

    done

--- api.ts
+++
@@ -1,8 +0,0 @@
-const baseUrl="http://localhost:3001/tasks"
-import { Task } from "./tasks/task"
-export const getAlltodos = async (): Promise<Task[]> =>
-{
-    const ret=await fetch(baseUrl);
-    const todo=await ret.json();
-    return todo;
-}
