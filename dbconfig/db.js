ed50ef2352726eed65076d93e4f05a7fa487e2a8
Hazeem5 <hazeemaulakh5@gmail.com.pk>
2024-09-10 16:18:01 +0500

    done

--- dbconfig/db.js
+++ dbconfig/db.js
@@ -1,12 +1,37 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-export  async function connect()
-{
+export async function connect() {
     try {
-        mongoose.connect(process.env.MONGO_URI)
-        mongoose.connection("connected",()=>{
-        console.log("connected to DB")})
+        // Connect to the MongoDB database
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log('Connected to DB');
+
+        // Add event listeners for the connection
+        mongoose.connection.on('error', (error) => {
+            console.error('Error connecting to DB:', error);
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            console.log('Disconnected from DB');
+        });
     } catch (error) {
-        console.log("error to connect to DB",error)
+        console.error('Failed to connect to DB:', error);
     }
 }
+
+// import mongoose from 'mongoose'
+
+// export  async function connect()
+// {
+//     try {
+//         mongoose.connect(process.env.MONGO_URI)
+//         mongoose.connection("connected",()=>{
+//         console.log("connected to DB")})
+//     } catch (error) {
+//         console.log("error to connect to DB",error)
+//     }
+// }
